cb65bbf3d6b9dcd6dec467ffb791abb0b6e6a3b5
Jude Cruz <jcruz@grokker.com>
2018-07-18 00:39:06 -0700

    attempt adding react to homepage

---
+++ like_button.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// sourced from https://cdn.rawgit.com/gaearon/0b180827c190fe4fd98b4c7f570ea4a8/raw/b9157ce933c79a4559d2aa9ff3372668cce48de7/LikeButton.js
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#react_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
