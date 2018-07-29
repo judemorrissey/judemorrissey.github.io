40c7a859a655e2a614fb772758e25d3ed8dd4657
Jude Morrissey <judemorrissey@users.noreply.github.com>
2018-07-28 20:44:04 -0700

    attempt to make whole page React root

---
+++ app.js
@@ -0,0 +1,19 @@
+import LikeButton from 'like_button';
+
+const e = React.createElement; // (component_name, props, children)
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return e('div', {className: 'mainContainer'},
+            e('h1', null, 'Hello world!'),
+            e(LikeButton),
+            e(LikeButton),
+            e(LikeButton)
+        );
+    }
+}
+
+ReactDOM.render(e(App), document.getElementById('root'));
--- like_button.js
+++ like_button.js
@@ -1,28 +1,19 @@
-'use strict';
-
 // sourced from https://cdn.rawgit.com/gaearon/0b180827c190fe4fd98b4c7f570ea4a8/raw/b9157ce933c79a4559d2aa9ff3372668cce48de7/LikeButton.js
 
-const e = React.createElement;
-
+const e = React.createElement; // (component_name, props, children)
 class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return e('button', { onClick: () => this.setState({ liked: true }) }, 'Like');
+    }
 }
 
-const domContainer = document.querySelector('#react_container');
-ReactDOM.render(e(LikeButton), domContainer);
-
+ReactDOM.render(e(LikeButton), document.getElementById('root'));
