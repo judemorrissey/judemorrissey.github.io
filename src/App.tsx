374a44d3884515b07959e8158e6d2b2db00b04cd
Jude Morrissey <judemorrissey@users.noreply.github.com>
2022-06-10 15:46:22 -0700

    front page now says under construction

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,8 @@ function App() {
       <header className="App-header">
         <img alt="logo" className="App-logo" src={logo} />
         <p>
-          {`Edit `}
-          <code>{`src/App.tsx`}</code>
-          {` and save to reload.`}
+          <code>{`Under Construction :)`}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank">
-          {`Learn React`}
-        </a>
       </header>
     </div>
   );
